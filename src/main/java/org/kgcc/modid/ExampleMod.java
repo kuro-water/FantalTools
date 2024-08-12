028c90edde98ffe7c402919211e139e99ced3747
kuro-water <kuro07001@gmail.com>
2024-08-12 18:11:26 +0900

    鉱石として地中に生成に成功（今回はエンドロッドを生成）

--- src/main/java/org/kgcc/modid/ExampleMod.java
+++ src/main/java/org/kgcc/modid/ExampleMod.java
@@ -1,6 +1,13 @@
 package org.kgcc.modid;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.feature.PlacedFeature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,6 +16,10 @@ public class ExampleMod implements ModInitializer {
     // ロガーの名前としてモッドIDを使用するのが最善の方法とされています。
     // そうすることで、どのモッドが情報、警告、エラーを書き込んだかが明確になります。
     public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+
+    // 鉱石を保存するための新しいクラス レベル を作成
+    public static final RegistryKey<PlacedFeature> CUSTOM_ORE_PLACED_KEY =
+            RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier("modid", "ore_custom"));
 
     @Override
     public void onInitialize() {
@@ -20,5 +31,9 @@ public class ExampleMod implements ModInitializer {
 
         TestItem.initialize();
         TestBlock.initialize();
+
+        // バイオームに機能を追加する
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(),
+                GenerationStep.Feature.UNDERGROUND_ORES, CUSTOM_ORE_PLACED_KEY);
     }
 }
