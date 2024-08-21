23cc49cb777b929047275ac3819cca3465a6267f
2423094-PC\2423094 <yetiantouyouli@gmail.com>
2024-08-21 22:21:10 +0900

    ツルハシの追加

--- src/main/java/org/kgcc/modid/TestItem.java
+++ src/main/java/org/kgcc/modid/TestItem.java
@@ -1,6 +1,7 @@
 package org.kgcc.modid;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.PickaxeItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -12,8 +13,12 @@ public class TestItem extends Item {
 
     private static final String MODID = System.getProperty("modid");
 
+    public static TutorialToolMaterial TUTORIAL_TOOL_MATERIAL = new TutorialToolMaterial();
+
     // an instance of our new item
     public static final Item testItem = register(new Item(new Item.Settings()), "test_item");
+
+    public static final Item tutorialPickaxe = register(new PickaxeItem(TUTORIAL_TOOL_MATERIAL,1,-1.0f, new Item.Settings()),"tutorial_pickaxe");
 
     public static Item register(Item instance, String path) {
         return Registry.register(Registries.ITEM, new Identifier(MODID, path), instance);
