a548c486c97dfd09316164ff77289ab044179772
kuro-water <kuro07001@gmail.com>
2024-09-16 16:03:50 +0900

    鉱石追加のためのコードが足りていなかったので修正

--- src/main/java/org/kgcc/modid/ExampleMod.java
+++ src/main/java/org/kgcc/modid/ExampleMod.java
@@ -1,8 +1,15 @@
 package org.kgcc.modid;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.feature.PlacedFeature;
 import org.kgcc.modid.registry.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +19,10 @@ public class ExampleMod implements ModInitializer {
     // ロガーの名前としてモッドIDを使用するのが最善の方法とされています。
     // そうすることで、どのモッドが情報、警告、エラーを書き込んだかが明確になります。
     public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+
+    // 鉱石を保存するための新しいクラス レベル を作成
+    public static final RegistryKey<PlacedFeature> CUSTOM_ORE_PLACED_KEY =
+            RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier("modid", "ore_custom"));
 
     @Override
     public void onInitialize() {
@@ -23,5 +34,8 @@ public class ExampleMod implements ModInitializer {
 
         ModItems.registerItems();
 
+        // バイオームに機能を追加する
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(),
+                GenerationStep.Feature.UNDERGROUND_ORES, CUSTOM_ORE_PLACED_KEY);
     }
 }
