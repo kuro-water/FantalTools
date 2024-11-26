181bd087d0aa8c8f9d02d38b97f06b491a3c4e16
kuro-water <kuro07001@gmail.com>
2024-11-26 18:57:32 +0900

    色々お試しなう

--- src/main/java/org/kgcc/fantalmod/mixin/InGameHudMixin.java
+++ src/main/java/org/kgcc/fantalmod/mixin/InGameHudMixin.java
@@ -27,6 +27,12 @@ public class InGameHudMixin {
     private static final int PIC_WIDTH = 386;
     private static final int PIC_HEIGHT = 123;
     
+    private static final int EFFECT_WIDTH = 18;
+    private static final int EFFECT_HEIGHT = 18;
+    private static final Identifier HUNGER = new Identifier(FantalMod.MODID, "textures/mob_effect/hunger.png");
+    
+    private static final Identifier PIC2 = new Identifier(FantalMod.MODID, "textures/mob_effect/320.jpg");
+    
     @Inject(method = "render", at = @At(("HEAD")))
     private void render(MatrixStack matrixStack, float tickDelta, CallbackInfo ci) {
         // ここはクライアントサイドかサーバーサイドかというと、クライアントサイドです。
@@ -36,21 +42,22 @@ public class InGameHudMixin {
             return;
         }
         
-        int x = client.getWindow().getScaledWidth() / 2;
-        int y = client.getWindow().getScaledHeight();
+        int scaledWidth = client.getWindow().getScaledWidth();
+        int mid = scaledWidth / 2;
+        int scaledHeight = client.getWindow().getScaledHeight();
 //            FantalModCommand.notifyAllPlayers(client.getServer(), "width: " + width + ", height: " + height);
         
         RenderSystem.setShader(GameRenderer::getPositionTexProgram);
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
         RenderSystem.setShaderTexture(0, EMPTY);
-        DrawableHelper.drawTexture(matrixStack, x - 94, y - 54, 0, 0, 91, 2, 12, 12);
+        DrawableHelper.drawTexture(matrixStack, mid - 94, scaledHeight - 54, 0, 0, 91, 2, 12, 12);
         
         RenderSystem.setShaderTexture(0, FULL);
         if (5 > 1) {
-            DrawableHelper.drawTexture(matrixStack, x - 94, y - 50, 0, 0, 182, 5, 182, 5);
+            DrawableHelper.drawTexture(matrixStack, mid - 94, scaledHeight - 50, 0, 0, 182, 5, 182, 5);
         }
         
-        final int mode = 2;
+        final int mode = 4;
         
         switch (mode) {
             case 1 -> {
@@ -69,6 +76,43 @@ public class InGameHudMixin {
                 final int pos = pollution_max / 2 - pollution / 2;
                 final int height = pollution / 2 + (PIC_HEIGHT - pollution_max / 2);
                 DrawableHelper.drawTexture(matrixStack, 0, pos, 0, pos, PIC_WIDTH, height, PIC_WIDTH, PIC_HEIGHT);
+            }
+            case 3 -> {
+                RenderSystem.setShaderTexture(0, HUNGER);
+                DrawableHelper.drawTexture(matrixStack, 0, 0, 0, 0, EFFECT_WIDTH * 2, EFFECT_HEIGHT * 2, EFFECT_WIDTH,
+                                           EFFECT_HEIGHT);
+                
+                
+                int red = 0xFFFF0000; // 赤色 (ARGB形式)
+                
+                
+                // ----- ホットバーの領域の描画 -----
+                int hotBarWidth = 182;
+                int hotBarHeight = 91;
+                
+                DrawableHelper.fill(matrixStack, mid - hotBarWidth / 2, scaledHeight - hotBarHeight / 2,
+                                    mid + hotBarWidth / 2, scaledHeight, red);
+                // ----- ホットバーの領域の描画終わり -----
+                
+                // 0からhotBarLeftの間にバーを描画できる
+                int hotBarLeft = mid - hotBarWidth / 2;
+                FantalMod.LOGGER.info("hotBarLeft: " + hotBarLeft);
+
+
+//                // 赤, 緑, 青, 黄, 紫, 水色
+//                List<Integer> color_list = List.of(0xFFFF0000, 0xFF00FF00, 0xFF0000FF, 0xFFFFFF00, 0xFFFF00FF,
+//                                                   0xFF00FFFF);
+//                int y = 200;
+//                for (int i = 0; i < 140; i += 10) {
+//                    DrawableHelper.fill(matrixStack, i, y, i + 10, y + 10, color_list.get(i % color_list.size()));
+//                }
+            }
+            case 4 -> {
+                RenderSystem.setShaderTexture(0, PIC2);
+                var size = 320 / 3;
+                var size2 = 320 / 4;
+                DrawableHelper.drawTexture(matrixStack, 0, 0, 0, 0, size, size, size, size);
+                DrawableHelper.drawTexture(matrixStack, size, 0, 0, 0, size2, size2, size2, size2);
             }
         }
     }
