408813c7e15862b1992f6f2137fa03102d0e920c
yururi-N <yetiantouyouli@gmail.com>
2025-03-05 22:50:27 +0900

    エラーあり

--- src/main/java/org/kgcc/fantalmod/FantalMod.java
+++ src/main/java/org/kgcc/fantalmod/FantalMod.java
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.feature.PlacedFeature;
---
+++ src/main/java/org/kgcc/fantalmod/mixin/GridWidgetAccessor.java
@@ -0,0 +1,14 @@
+package org.kgcc.fantalmod.mixin;
+
+import net.minecraft.client.gui.widget.ClickableWidget;
+import net.minecraft.client.gui.widget.GridWidget;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.List;
+
+@Mixin(GridWidget.class)
+public interface GridWidgetAccessor {
+    @Accessor
+    List<ClickableWidget> getChildren();
+}
