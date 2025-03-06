34096bb173cd888d96ab6fcd7e3b9ea9e499d593
kuro-water <kuro07001@gmail.com>
2025-03-06 23:32:27 +0900

    方角によるエラー（上とか下とかが入ってしまう）を、ctx.getHorizontalPlayerFacing().getOpposite()によって修正した hasRecipeでスロット2を使う際の比較対象がFantalNuggetになっていたので、FantalOreにsひゅう制

--- src/main/java/org/kgcc/fantalmod/gui/GemInfusingBlockEntity.java
+++ src/main/java/org/kgcc/fantalmod/gui/GemInfusingBlockEntity.java
@@ -7,7 +7,6 @@ import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventories;
 import net.minecraft.inventory.SimpleInventory;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.screen.NamedScreenHandlerFactory;
@@ -22,119 +21,125 @@ import org.kgcc.fantalmod.registry.FantalModItems;
 
 public class GemInfusingBlockEntity extends BlockEntity implements NamedScreenHandlerFactory, ImplementedInventory {
     private final DefaultedList<ItemStack> inventory = DefaultedList.ofSize(3, ItemStack.EMPTY);
-
+    
     protected final PropertyDelegate propertyDelegate;
     private int progress = 0;
     private int maxProgress = 72;
-
+    
     public GemInfusingBlockEntity(BlockPos pos, BlockState state) {
         super(ModBlockEntities.GEM_INFUSING_STATION, pos, state);
         this.propertyDelegate = new PropertyDelegate() {
             public int get(int index) {
                 switch (index) {
-                    case 0: return GemInfusingBlockEntity.this.progress;
-                    case 1: return GemInfusingBlockEntity.this.maxProgress;
-                    default: return 0;
+                    case 0:
+                        return GemInfusingBlockEntity.this.progress;
+                    case 1:
+                        return GemInfusingBlockEntity.this.maxProgress;
+                    default:
+                        return 0;
                 }
             }
-
+            
             public void set(int index, int value) {
-                switch(index) {
-                    case 0: GemInfusingBlockEntity.this.progress = value; break;
-                    case 1: GemInfusingBlockEntity.this.maxProgress = value; break;
+                switch (index) {
+                    case 0:
+                        GemInfusingBlockEntity.this.progress = value;
+                        break;
+                    case 1:
+                        GemInfusingBlockEntity.this.maxProgress = value;
+                        break;
                 }
             }
-
+            
             public int size() {
                 return 2;
             }
         };
     }
-
+    
     @Override
     public DefaultedList<ItemStack> getItems() {
         return this.inventory;
     }
-
+    
     @Override
     public Text getDisplayName() {
         return Text.literal("Gem Infusing Station");
     }
-
+    
     @Nullable
     @Override
     public ScreenHandler createMenu(int syncId, PlayerInventory inv, PlayerEntity player) {
         return new GemInfusingScreenHandler(syncId, inv, this, this.propertyDelegate);
     }
-
+    
     @Override
     protected void writeNbt(NbtCompound nbt) {
         super.writeNbt(nbt);
         Inventories.writeNbt(nbt, inventory);
         nbt.putInt("gem_infusing_station.progress", progress);
     }
-
+    
     @Override
     public void readNbt(NbtCompound nbt) {
         Inventories.readNbt(nbt, inventory);
         super.readNbt(nbt);
         progress = nbt.getInt("gem_infusing_station.progress");
     }
-
+    
     private void resetProgress() {
         this.progress = 0;
     }
-
+    
     public static void tick(World world, BlockPos blockPos, BlockState state, GemInfusingBlockEntity entity) {
-        if(world.isClient()) {
+        if (world.isClient()) {
             return;
         }
-
-        if(hasRecipe(entity)) {
+        
+        if (hasRecipe(entity)) {
             entity.progress++;
             markDirty(world, blockPos, state);
-            if(entity.progress >= entity.maxProgress) {
+            if (entity.progress >= entity.maxProgress) {
                 craftItem(entity);
             }
         } else {
             entity.resetProgress();
             markDirty(world, blockPos, state);
         }
-
     }
-
+    
     private static void craftItem(GemInfusingBlockEntity entity) {
         SimpleInventory inventory = new SimpleInventory(entity.size());
         for (int i = 0; i < entity.size(); i++) {
             inventory.setStack(i, entity.getStack(i));
         }
-
-        if(hasRecipe(entity)) {
+        
+        if (hasRecipe(entity)) {
             entity.removeStack(1, 1);
             //もとになる素材をから出てくるやつ（多分
             entity.setStack(2, new ItemStack(FantalModItems.FANTAL_ORE,
-                    entity.getStack(2).getCount() + 1));
-
+                                             entity.getStack(2).getCount() + 1));
+            
             entity.resetProgress();
         }
     }
-
+    
     private static boolean hasRecipe(GemInfusingBlockEntity entity) {
         SimpleInventory inventory = new SimpleInventory(entity.size());
         for (int i = 0; i < entity.size(); i++) {
             inventory.setStack(i, entity.getStack(i));
         }
-    //元になる素材（多分
+        //元になる素材（多分
         boolean hasRawGemInFirstSlot = entity.getStack(1).getItem() == FantalModItems.ROW_FANTAL;
-
+        
         return hasRawGemInFirstSlot && canInsertAmountIntoOutputSlot(inventory)
-                && canInsertItemIntoOutputSlot(inventory, FantalModItems.FANTAL_NUGGET);
+                && canInsertItemIntoOutputSlot(inventory, FantalModItems.FANTAL_ORE.asItem());
     }
-
+    
     private static boolean canInsertItemIntoOutputSlot(SimpleInventory inventory, Item output) {
         return inventory.getStack(2).getItem() == output || inventory.getStack(2).isEmpty();
     }
-
+    
     private static boolean canInsertAmountIntoOutputSlot(SimpleInventory inventory) {
         return inventory.getStack(2).getMaxCount() > inventory.getStack(2).getCount();
     }
--- src/main/java/org/kgcc/fantalmod/gui/GemInfusingStationBlock.java
+++ src/main/java/org/kgcc/fantalmod/gui/GemInfusingStationBlock.java
@@ -20,79 +20,80 @@ import org.jetbrains.annotations.Nullable;
 
 public class GemInfusingStationBlock extends BlockWithEntity implements BlockEntityProvider {
     public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
-
+    
     public GemInfusingStationBlock(Settings settings) {
         super(settings);
     }
-
+    
     private static VoxelShape SHAPE = Block.createCuboidShape(0, 0, 0, 16, 10, 16);
-
+    
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
         return SHAPE;
     }
-
+    
     @Nullable
     @Override
     public BlockState getPlacementState(ItemPlacementContext ctx) {
-        return this.getDefaultState().with(FACING, ctx.getPlayerLookDirection());
+        return this.getDefaultState().with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
     }
-
+    
     @Override
     public BlockState rotate(BlockState state, BlockRotation rotation) {
         return state.with(FACING, rotation.rotate(state.get(FACING)));
     }
-
+    
     @Override
     public BlockState mirror(BlockState state, BlockMirror mirror) {
         return state.rotate(mirror.getRotation(state.get(FACING)));
     }
-
+    
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
         builder.add(FACING);
     }
-
-
+    
+    
     /* BLOCK ENTITY */
-
+    
     @Override
     public BlockRenderType getRenderType(BlockState state) {
         return BlockRenderType.MODEL;
     }
-
+    
     @Override
     public void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
         if (state.getBlock() != newState.getBlock()) {
             BlockEntity blockEntity = world.getBlockEntity(pos);
             if (blockEntity instanceof GemInfusingBlockEntity) {
-                ItemScatterer.spawn(world, pos, (GemInfusingBlockEntity)blockEntity);
-                world.updateComparators(pos,this);
+                ItemScatterer.spawn(world, pos, (GemInfusingBlockEntity) blockEntity);
+                world.updateComparators(pos, this);
             }
             super.onStateReplaced(state, world, pos, newState, moved);
         }
     }
-
+    
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos,
-                              PlayerEntity player, Hand hand, BlockHitResult hit) {
+                              PlayerEntity player, Hand hand, BlockHitResult hit
+                             ) {
         if (!world.isClient) {
             NamedScreenHandlerFactory screenHandlerFactory = state.createScreenHandlerFactory(world, pos);
-
+            
             if (screenHandlerFactory != null) {
                 player.openHandledScreen(screenHandlerFactory);
             }
         }
-
+        
         return ActionResult.SUCCESS;
     }
-
+    
     @Nullable
     @Override
     public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
         return new GemInfusingBlockEntity(pos, state);
     }
-
+    
     @Nullable
     @Override
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
