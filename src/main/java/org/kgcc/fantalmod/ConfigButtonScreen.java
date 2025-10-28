745f915c266aa2b8bf1bf43e5b9120855b3df315
yururi-N <yetiantouyouli@gmail.com>
2025-10-29 08:47:18 +0900

    UIマルチ対応できたはず

--- src/main/java/org/kgcc/fantalmod/ConfigButtonScreen.java
+++ src/main/java/org/kgcc/fantalmod/ConfigButtonScreen.java
@@ -1,16 +1,19 @@
 package org.kgcc.fantalmod;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.SliderWidget;
 import net.minecraft.client.gui.widget.ClickableWidget;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Mixin(MultiplayerScreen.class)
 public class ConfigButtonScreen extends Screen {
     private final Screen parent; //Ecsメニュー画面を保存
 
