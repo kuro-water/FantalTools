7445cacf7aa1b6c91692def17d7d453825d7434d
kuro-water <kuro07001@gmail.com>
2025-10-30 19:15:09 +0900

    NoneSkillがツールチップに表示されなかったので修正

--- src/main/java/org/kgcc/fantalmod/skill/NoneSkill.java
+++ src/main/java/org/kgcc/fantalmod/skill/NoneSkill.java
@@ -1,10 +1,7 @@
 package org.kgcc.fantalmod.skill;
 
-import net.minecraft.client.item.TooltipContext;
-import net.minecraft.item.ItemStack;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
-import net.minecraft.world.World;
 
 import java.util.List;
 
@@ -24,9 +21,5 @@ public class NoneSkill implements BaseSkill {
     @Override
     public MutableText getName() {
         return Text.translatable("skill.fantalmod.none");
-    }
-    
-    @Override
-    public void appendTooltip(ItemStack stack, World world, java.util.List<Text> tooltip, TooltipContext context) {
     }
 }
